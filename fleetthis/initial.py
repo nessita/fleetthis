565c50ab45804418bf1c2b65d012044464aabfd2
Natalia Bidart <nataliabidart@gmail.com>
2012-08-27 17:51:17 -0300

    Now it works!

--- fleetthis/initial.py
+++ fleetthis/initial.py
@@ -1,20 +1,24 @@
 from os import environ
 environ['DJANGO_SETTINGS_MODULE'] = 'fleetthis.settings'
 
+from decimal import Decimal
+
 from django.contrib.auth.models import User
 from fleetthis.fleetcore.models import Plan, Phone
 
 
-## (('XMD01', 2200, True), ('INTRC', 9507, False),
-##  ('TCL16', 2900, False), ('', 0, False))
-plans = {}
-for i, j, k in (('TCL16', Decimal('27.00'), True),):
-    plan[i] = Plan.objects.create(name=i, price=j, with_clearing=k)
-
-
-User.objects.create_superuser(
-    username='admin', email='fleetthis@gmail.com', password='123456')
-
+all_plans = (
+    # ('XMD01', 2200, True), ('INTRC', 9507, False),
+    # ('TCL16', 2900, False), ('', 0, False))
+    ('TCL16', Decimal('35.00'), True, 130, Decimal('0.27'), Decimal('0.1'),
+     """DETALLE PLAN DE PRECIO: PLAN TCL16 - Abono: $ 35.00 Pesos
+Libres en el Plan:$ 35.00
+Precios sin imp ni cargo ENARD Ley 26573
+Min. destino a teléfonos fijos: $ 0.27 $ 0.27
+Min. destino a móviles del cliente: $ 0.00 $ 0.00
+Min. destino a móviles: $0.27 $ 0.27
+Los SMS no están incluídos en el abono, cada uno sale $ 0.10"""),
+)
 leaders = (
     ('Anthony', 'Lenton', 'antoniolenton@gmail.com'),
     ('Kuka', 'Moroni', 'karimoroni@gmail.com'),
@@ -23,15 +27,6 @@ leaders = (
     ('Natalia', 'Bidart', 'nataliabidart@gmail.com'),
     ('Walter', 'Alini', 'walteralini@gmail.com')
 )
-users = {}
-for fn, ln, email in leaders:
-    username = ('%s%s' % (fn[0], ln)).lower()
-    users[username] = User.objects.create(
-        username=username, email=email, first_name=fn, last_name=ln,
-        password=User.objects.make_random_password(),
-    )
-
-
 phones = (
     ('Anthony', 'Vale', 1166936420),
     ('Anthony', 'ma de Vale', 2314447229),
@@ -39,25 +34,47 @@ phones = (
     ('Anthony', 'Miguel Berdión', 2314516976),
     ('Anthony', 'Cuñada Vale', 2914298833),
     ('Kuka', 'Kuka', 3512255432),
-    ('Nati', 'Mati', 3512362650),
-    ('Waldo', 'Adrián Alini', 3513290201),
-    ('Waldo', 'Aldo Alini', 3513290204),
-    ('Waldo', 'Marianela Terragni', 3513290207),
-    ('Mati', 'Lucía', 3513456948),
+    ('Natalia', 'Mati', 3512362650),
+    ('Walter', 'Adrián Alini', 3513290201),
+    ('Walter', 'Aldo Alini', 3513290204),
+    ('Walter', 'Marianela Terragni', 3513290207),
+    ('Nelson', 'Lucía', 3513456948),
     ('Anthony', 'Anthony', 3513500734),
-    ('Waldo', 'Waldo', 3513901750),
-    ('Nati', 'Naty', 3513901899),
-    ('Waldo', 'Adriana Spiazzi', 3516624678),
-    ('Waldo', 'Franco Alini', 3516624706),
-    ('Mati', 'Sofía', 3516656710),
-    ('Mati', 'Nelson', 3516656711),
+    ('Walter', 'Walter', 3513901750),
+    ('Natalia', 'Naty', 3513901899),
+    ('Walter', 'Adriana Spiazzi', 3516624678),
+    ('Walter', 'Franco Alini', 3516624706),
+    ('Nelson', 'Sofía', 3516656710),
+    ('Nelson', 'Nelson', 3516656711),
     ('Kuka', 'Damián', 3516656713),
-    ('Waldo', 'Mirta Arnoletti', 3516847977),
-    ('Waldo', 'Andrea Spiazzi', 3516847979),
+    ('Walter', 'Mirta Arnoletti', 3516847977),
+    ('Walter', 'Andrea Spiazzi', 3516847979),
 )
 
 
-tcl16 = plan['TCL16']
-for leader, notes, number in phones:
-    u = Leader.objects.get(first_name=leader)
-    Phone.objects.create(number=number, user=u, notes=notes, plan=tcl16)
+def main():
+    plans = {}
+    for i, j, k, l, min_price, sms_price, desc in all_plans:
+        plans[i] = Plan.objects.create(name=i, price=j, with_clearing=k,
+                                       minutes=l, min_price=min_price,
+                                       sms_price=sms_price, description=desc)
+
+    User.objects.create_superuser(
+        username='admin', email='fleetthis@gmail.com', password='admin')
+
+    users = {}
+    for fn, ln, email in leaders:
+        username = ('%s%s' % (fn[0], ln)).lower()
+        users[username] = User.objects.create(
+            username=username, email=email, first_name=fn, last_name=ln,
+            password=User.objects.make_random_password(),
+        )
+
+    tcl16 = plans['TCL16']
+    for leader, notes, number in phones:
+        u = User.objects.get(first_name=leader)
+        Phone.objects.create(number=number, user=u, notes=notes, plan=tcl16)
+
+
+if __name__ == '__main__':
+    main()
