86182779c7159078b27986ba067d5e05705b0852
Matias Bordese <mbordese@gmail.com>
2012-09-07 15:05:08 -0300

    Improved consumption admin.

--- fleetthis/fleetcore/admin.py
+++ fleetthis/fleetcore/admin.py
@@ -17,8 +17,35 @@ from fleetthis.fleetcore.models import (
 
 
 admin.site.register(Bill)
-admin.site.register(Consumption)
 admin.site.register(Fleet)
 admin.site.register(Phone)
 admin.site.register(Plan)
 admin.site.register(UserProfile)
+
+
+class ConsumptionAdmin(admin.ModelAdmin):
+    """Admin class for Consumption."""
+    search_fields = ('phone__user__username', 'phone__user__first_name',
+                     'phone__user__last_name', 'bill__billing_date',)
+    list_filter = ('bill', 'phone',)
+    fieldsets = (
+        (None, {
+            'fields': ('phone', 'bill',)
+        }),
+        ('Totals', {
+            'fields': ('total_before_taxes', 'taxes', 'total_before_round',
+                       ('total', 'payed'),)
+        }),
+        ('Factura', {
+            'classes': ('collapse',),
+            'fields': ('reported_user', 'reported_plan', 'monthly_price',
+                       ('services', 'refunds'), 'included_min',
+                       ('exceeded_min', 'exceeded_min_price'),
+                       ('ndl_min', 'ndl_min_price'),
+                       ('idl_min', 'idl_min_price'), ('sms', 'sms_price'),
+                       ('equipment_price', 'other_price'), 'reported_total',
+                      )
+        }),
+    )
+
+admin.site.register(Consumption, ConsumptionAdmin)
