33180e7a4d93320b6f545615475d4ffb27bba45d
nessita <nataliabidart@gmail.com>
2012-09-30 23:59:54 -0300

    Proper creation of dummy bills.

--- fleetcore/tests/factory.py
+++ fleetcore/tests/factory.py
@@ -56,7 +56,7 @@ class Factory(object):
         return self.make_something(Fleet, default, **kwargs)
 
     def make_bill(self, **kwargs):
-        default = dict(fleet=self.make_fleet(), invoice='zaraza.pdf')
+        default = dict(fleet=self.make_fleet(), invoice='invoices/zaraza.pdf')
         return self.make_something(Bill, default, **kwargs)
 
     def make_plan(self, **kwargs):
