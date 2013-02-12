0aaee58513518b2dd3b3621b923231be21d177f6
Matias Bordese <mbordese@gmail.com>
2013-02-12 14:33:57 -0300

    Added password reset explicit steps/templates.

--- fleetusers/urls.py
+++ fleetusers/urls.py
@@ -16,9 +16,13 @@ urlpatterns = patterns(
         dict(template_name='users/password_reset.html',
              post_reset_redirect='/'),
         name='password-reset'),
+    url(r'^password-reset/done/$', 'password_reset_done',
+        dict(template_name='users/password_reset_done.html')),
     url(r'^password-reset/(?P<uidb36>[\w-]+)/(?P<token>[\w-]+)/confirm/$',
         'password_reset_confirm',
         dict(template_name='users/password_reset_confirm.html',
              post_reset_redirect='/'),
         name='password-reset-confirm'),
+    url(r'^password-reset/complete/$', 'password_reset_complete',
+        dict(template_name='users/password_reset_complete.html')),
 )
